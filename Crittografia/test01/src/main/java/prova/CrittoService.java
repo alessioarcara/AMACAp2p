548dae0af7f85240a428122c580a6c85bf7418bb
Alessio Arcara <diaclo@MacBook-Pro.local>
2020-07-01 07:33:19 +0200

    cifratura c=m^e mod n aggiunto da SISTEMARE

--- Crittografia/test01/src/main/java/prova/CrittoService.java
+++ Crittografia/test01/src/main/java/prova/CrittoService.java
@@ -8,23 +8,29 @@ import java.math.BigInteger;
 
 public class CrittoService extends JavaService {
 
+    // Problema: il ciphertext generato dall'algoritmo RSA è di lunghezza diverso per la diversa lunghezza del plaintext
+    // Soluzione da trovare: generare ciphertext di lunghezza standard
+
     public BigInteger Euclide(BigInteger toziente){
+
+        System.out.println("");
+        System.out.println("** inizializzazione algoritmo di Euclide **");
 
         BigInteger t = toziente;
         BigInteger e = BigInteger.valueOf(2);
         BigInteger r = new BigInteger("0");
 
         //il massimo comune divisore di un coprimo con F(n) è 1; esco solo quando lo trovo
-        while(r != BigInteger.valueOf(1)){
+        while(!(r.equals(BigInteger.ONE))){
 
             r = e.gcd(t);
             System.out.println("resto: " + r);
 
-            if(r == BigInteger.valueOf(1) ) {
+            if(r.equals(BigInteger.ONE)){
                 break;
             }
 
-            e.add(BigInteger.valueOf(1));
+            e = e.add(BigInteger.ONE);
             System.out.println("contatore: " + e);
         }
         return e;
@@ -39,9 +45,15 @@ public class CrittoService extends JavaService {
         BigInteger n;
         BigInteger toziente;
         //sistemare questo;
-        BigInteger uno = BigInteger.valueOf(1);
+        BigInteger uno = BigInteger.ONE;
+        BigInteger e;
+        BigInteger c;
 
-        int lengthRSA = 2048;
+        BigInteger m;
+        byte [] ms = s;
+        System.out.println(Arrays.toString(ms));
+
+        int lengthRSA = 1024;
 
         Random random = new Random();
 
@@ -52,11 +64,15 @@ public class CrittoService extends JavaService {
         n = p.multiply(q);
         toziente = (p.subtract(uno)).multiply(q.subtract(uno));
 
-        Euclide(toziente);
+        e = Euclide(toziente);
 
-        System.out.println(n);
+        //cifratura per singola cifra o intero messaggio? 
+        m = new BigInteger(s);
 
-        return p;
+        //ottimizzare cifratura con aritmetica modulo
+        c = m.modPow(e, n);
+
+        return c;
     }
 
     public Value encrypting( Value request ) {
@@ -65,9 +81,10 @@ public class CrittoService extends JavaService {
         System.out.println("Il messaggio inserito è: " + message );
         byte[] byteArr = message.getBytes();
         System.out.println("Il messaggio in bytes è: " + Arrays.toString(byteArr));
-        RSA(byteArr);
+
         Value response = Value.create();
-        response.getFirstChild( "reply" ).setValue(new String(byteArr));
+        String messaggioCriptato = RSA(byteArr).toString();
+        response.getFirstChild( "reply" ).setValue(messaggioCriptato);
         return response;
     }
 }
